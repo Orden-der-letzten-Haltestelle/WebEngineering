95bcdd314352926f31ce34e913dfa7f87629076f
Leon Salenbacher <leon@salenbacher.com>
2025-06-08 18:00:38 +0200

    added createdAt to WishlistMember

--- app/backend/src/objects/user/WishlistMember.js
+++ app/backend/src/objects/user/WishlistMember.js
@@ -1,4 +1,3 @@
-
 import BasicUser from "./BasicUser"
 export default class WishlistMember extends BasicUser {
     /**
@@ -6,17 +5,12 @@ export default class WishlistMember extends BasicUser {
      * @param {int} id
      * @param {string} name
      * @param {string} email
+     * @param {Date} createdAt
      * @param {int} userWishlistRelationId
      * @param {Array<WishlistRoles>} roles
      */
-    constructor(
-        id,
-        name,
-        email,
-        userWishlistRelationId,
-        roles
-    ) {
-        super(id, name, email)
+    constructor(id, name, email, createdAt, userWishlistRelationId, roles) {
+        super(id, name, email, createdAt)
         this.userWishlistRelationId = userWishlistRelationId
         this.roles = roles
     }
