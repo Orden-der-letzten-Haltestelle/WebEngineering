a05f388c3d88393204dc571f2f47798ab869864a
Leon Salenbacher <leon@salenbacher.com>
2025-06-25 18:12:46 +0200

    setpu

---
+++ app/backend/src/controllers/wishlist.controller.js
@@ -0,0 +1,10 @@
+import WishlistRoles from "../objects/user/WishlistRoles";
+
+/**
+ * Proofs, if the given user has the rights to access the wishlist
+ * @param {string} userId 
+ * @param {WishlistRoles} wishlistRole 
+ */
+async function verifyWishlistRole(userId, wishlistRole, wishlistId) {
+    
+}
---
+++ app/backend/src/routes/wishlist.routes.js
@@ -0,0 +1,7 @@
+import express from "express"
+const router = express.Router()
+
+import AuthController from "../controllers/auth.controller.js"
+import WishlistRoles from "../objects/user/WishlistRoles.js"
+
+router.get("/", )
