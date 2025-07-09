1b39dead191abfaf4d5e50c54dfce55c563e2bf7
Leon Salenbacher <leon@salenbacher.com>
2025-07-09 09:04:45 +0200

    fixed some issues

--- app/frontend/api/productApiHandler.js
+++ app/frontend/api/productApiHandler.js
@@ -21,3 +21,21 @@ export async function fetchProducts(token) {
     }
     return res.json()
 }
+
+/**
+ * Fetch products information from backend
+ * @param {string} token
+ * @return {Promise<Object>}
+ * @throws {ApiError}
+ */
+
+export async function fetchProductById(productId) {
+    const res = await fetch(`${config.host}/products/${productId}`);
+
+    if (!res.ok) {
+        const errorData = await res.json()
+        const errorMessage = errorData.message || "Failed to fetch product"
+        throw new ApiError(errorMessage, res.status, errorData)
+    }
+    return await res.json();
+}
--- app/frontend/pages/products/productDetailled/ProductDetailled.js
+++ app/frontend/pages/products/productDetailled/ProductDetailled.js
@@ -1,11 +1,17 @@
-import { fetchProductById } from "../../../api/productDetailledApiHandler.js"
+import { fetchProductById } from "../../../api/productApiHandler.js"
 
-export default async function ProductDetailledLoader({ params }) {
-  const productId = params.productId;
+export default async function ProductDetailledLoader(req, res) {
+  const productId = req.params.productId;
   const product = await fetchProductById(productId);
+
+  //proof if admin is connected 
+  const isAdmin = req?.user?.isAdmin
+console.log(req.user)
+
 
   return {
     title: product.name,
-    product: product
+    product: product,
+    isAdmin: isAdmin
   };
 }
