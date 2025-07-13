b125357b426bf971c11d7c73b92c67e465a7182d
SOPHIE DUERR <sophie.duerr@mercedes-benz.com>
2025-07-13 15:17:36 +0200

    pictures and new testdata (products)

--- db/testdata.sql
+++ db/testdata.sql
@@ -32,10 +32,15 @@ INSERT INTO webshop.User_has_Role (userId, roleId) VALUES
 
 -- Produkte
 INSERT INTO webshop.Products (name, description, amount, price) VALUES
-  ('T-Shirt', 'Schwarzes T-Shirt, Größe M', 100, 1999),
-  ('Hoodie', 'Grauer Hoodie, Größe L', 50, 3999),
+  ('T-Shirt', 'T-Shirt Größe M, zeigt eine Abstraktion unseres Heiligen Sankt Negroberts mit Initialen', 100, 1999),
+  ('Hoodie', 'Grauer Hoodie Größe L, zeigt die Initialen unseren Heiligen Sankt Negroberts', 50, 3999),
   ('Cap', 'Baseball Cap, schwarz', 200, 1499),
-  ('Sneaker', 'Weiße Sneaker, Größe 42', 30, 6999);
+  ('Sneaker', 'Weiße Sneaker, Größe 42', 30, 6999),
+  ('Tasse', 'Tasse für den besonderen Genuß von Heißgetränken und Erinnerungsstück an den Heiligen', 150, 1099),
+  ('Schlüsselanhänger', 'Für den stetigen Begleiter im Alltag', 150, 599),
+  ('Aufkleber', 'Zur Verbreitung der frohen Kunde', 250, 299),
+  ('Handyhülle', 'Für den stetigen Begleiter im Alltag und als Schutz vor schädlicher Strahlung des Mobilgeräts', 80, 1999),
+  ('Kissen', 'Als Verschönerung des Wohnraums und Bequemlichkeit', 50, 2599);
 
 -- Warenkorb-Einträge
 INSERT INTO webshop.CartItems (userId, productId, amount, bought, boughtAt, addedAt) VALUES
